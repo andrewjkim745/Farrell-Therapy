ab7dc0ecd590262bf48d4d122aae6756cf4da7b7
Andrew Kim <andrewk745@gmail.com>
2022-02-08 02:08:51 -0500

    added education data object

--- src/assets/cvdata.js
+++ src/assets/cvdata.js
@@ -1,4 +1,33 @@
-export const data = [
+
+export const education = [
+    {
+        date: '12/07',
+        place: 'Diplomate (Board Certification) in Group Psychology'
+    },
+    {
+        date: '4/07',
+        place: 'Adelphi University, Garden City, New York Post-Doctoral Certification in Group Psychotherapy'
+    },
+    {
+        date: '6/98',
+        place: 'Adelphi University, Garden City, New York Post-Doctoral Certification in Psychoanalysis and Psychotherapy'
+    },
+    {
+        date: '1/90',
+        place: 'Long Island University, Brooklyn, New York Ph.D. Clinical Psychology'
+    },
+    {
+        date: '6/80',
+        place: 'City College of the City University of New York, New York, New York M.A. Program in Physiological Psychology'
+    },
+    {
+        date: '1/77',
+        place: 'City College of the City University of New York, New York, New York B.A. Psychology'
+    }
+]
+
+
+export const cvdata = [
     {
         date:'9/90-Present',
         type: 'Private Practice',
@@ -12,6 +41,15 @@ export const data = [
         bullets: ['Supervise and oversee all clinical materials for students', 'Oversee academic, administrative, and budgetary aspects of Center', 'Conduct case conferences and seminars', 'Screen all prospective patients to Center and assign to Therapist', 'Evaluate ongoing progress of students with Program Directors']
     },
     {
-        
-    }
+        date: '6/96 - 12/16',
+        type: 'Consulting Psychologist',
+        location: 'Ozanam Hall of Queens Nursing Home 42-41 201st Street Bayside, New York 11357',
+        bullets: ['Diagnose and evaluate residents’ level of psychopathology and their appropriateness for psychotherapy', 'Provide individual psychotherapy to residents who suffer from depression, adjustment disorders, and other maladies associated with the aged', 'Consult with family members of residents to help them better cope with the struggles and stresses of having a family member in a nursing facility and accepting the realities and limitations of such a life', 'Conduct in-service seminars with all staff to increase awareness of psychopathology and appropriate response to it']
+    },
+    {
+        date: '9/07 - Present',
+        type: 'Adjunct Professor',
+        location: 'Post Graduate Program in Psychoanalysis and Psychotherapy (Adult) Derner Institute Adelphi University Garden City, New York 11530',
+        bullets: ['Conduct theory based courses emphasizing the works of Klein, Bion, Fairbairn, and other object relations/relational theorists (Class Title: British Object Relations Theorists)', 'Conducted case conference seminars designed to sharpen students clinical skills and highlight the juncture between theory and practice', 'Conduct ongoing process groups to illuminate and demonstrate the technique and theoretical underpinnings of group psychoanalysis', 'Participate in staff meetings to modify and adapt course work, identify areas of need, and evaluate the progress of students']
+    }   
 ]
